0a7db899ffd79cef4fa6aac3b7f097143c5fe3ee
cerebus <adrogunova@gmail.com>
2021-05-16 20:47:36 +0300

    made one half of ft_write

--- main.c
+++ main.c
@@ -6,6 +6,7 @@ ssize_t ft_write(int fildes, const void *buf, size_t nbyte);
 
 int main() {
     printf("Hello, World!\n");
-    ft_write(1, "hi asm!", 7);
+    int i =  (int)ft_write(1, "hi asm!\n", 8);
+    printf("i = %d", i);
     return 0;
 }
