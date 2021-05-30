7fd37a8298c44da8956ecfb9205565712329ee80
cerebus <adrogunova@gmail.com>
2021-05-30 20:49:07 +0300

    TODO: - add errno and error parsing to ft_strdup.s

--- ft_strdup.s
+++ ft_strdup.s
@@ -9,8 +9,13 @@ _ft_strdup:
             call _ft_strlen
             mov rdi, rax
             inc rdi
+            ;
+        ;    mov rdi, 0
+            ;
             call _malloc
             jz malloc_err
+           ; cmp rax, 0
+         ;   je malloc_err
             mov rdi, rax
             pop rsi
             call _ft_strcpy
@@ -18,4 +23,5 @@ _ft_strdup:
             ret
 malloc_err:
            pop rdi
+           mov rax, 0
            ret
