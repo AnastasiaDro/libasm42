2a5689c5056a41a1ebe04338fc233142fe32ea02
cerebus <adrogunova@gmail.com>
2021-05-30 19:18:00 +0300

    added all tests except errno cases and ft_read test, because ft_read does not work
    TODO:
    -fix ft_read
    -errno tests
    -the norm (?)

--- main.c
+++ main.c
@@ -2,12 +2,14 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <strings.h>
+#include <sys/fcntl.h>
 
 ssize_t ft_write(int fildes, const void *buf, size_t nbyte);
 size_t ft_strlen(const char *s);
 char    *ft_strcpy(char *dst, char *src);
 int     ft_strcmp(const char *s1, const char *s2);
 char    *ft_strdup(const char *src);
+int     ft_read(int fildes, void *buf, size_t nbyte);
 
 
 int main()
@@ -59,21 +61,83 @@ int main()
 	free(my_dst);
 	free(orig_dist);
 
-	printf("\n\n	FT_STRCMP\n");
-	char *s1 = "12345";
-	char *s2 = "123";
-	printf("  		      original: |    my:\n");
-	printf("strings: %s & %s   ", s1, s2);
-    printf("  %d          %d\n", strcmp(s1, s2), ft_strcmp(s1, s2));
-    printf("test 2:\n %d\n", ft_strcmp("123", "12345"));
-    printf("test 3:\n %d\n", ft_strcmp("123", ""));
-    printf("test 4:\n %d\n", ft_strcmp("12345", "12345"));
+    printf("\n\n	FT_STRCMP\n");
+    char *s1 = "12345";
+    char *s2 = "123";
+    printf("TEST 1. STRINGS: %s & %s   \n", s1, s2);
+	printf(" original: |    my:\n");
+    printf("     %d        %d\n", strcmp(s1, s2), ft_strcmp(s1, s2));
+    printf("\nTEST 2. STRINGS: %s & %s   \n", s2, s1);
+    printf(" original: |    my:\n");
+    printf("     %d        %d\n", strcmp(s2, s1), ft_strcmp(s2, s1));
+    s1 = "";
+    s2 = "0";
+    printf("\nTEST 3. STRINGS: >>EMPTY<< %s& %s   \n", s1, s2);
+    printf(" original: |    my:\n");
+    printf("     %d        %d\n", strcmp(s1, s2), ft_strcmp(s1, s2));
 
-    printf("\nft_strdup\n");
+    s1 = "12345";
+    s2 = "12345";
+    printf("\nTEST 4. STRINGS: %s & %s \n", s1, s2);
+    printf(" original: |    my:\n");
+    printf("     %d           %d\n", strcmp(s1, s2), ft_strcmp(s1, s2));
+
+    printf("\n\n	FT_STRDUP\n");
     char *src = "12345";
-    char *s = ft_strdup(src);
-    printf("test 1:\ns = %s\n", s);
-    free(s);
-    printf("src after freeing: %s\n", src);
+    char *my_res = ft_strdup(src);
+    char *their_res = strdup(src);
+    printf("TEST 1. STRING: %s\n", src);
+    printf(" original: |    my:\n");
+    printf("  %s        %s\n", their_res, my_res);
+    free(my_res);
+    free(their_res);
+
+    src = "";
+    printf("\nTEST 2. STRING: >>EMPTY<<%s\n", src);
+    char *m_res = ft_strdup(src);
+    char *t_res = strdup(src);
+    printf(" original: |    my:\n");
+    printf("  %s        %s\n", t_res, m_res);
+    free(m_res);
+    free(t_res);
+
+    char *l_s = "_1_01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz_2_01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz_3_01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz_4_01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz_5_01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    printf("\nTEST 3. LONG STRING: %s\n\n", l_s);
+    char *m_r = ft_strdup(l_s);
+    char *t_r = strdup(l_s);
+    printf("original: \n%s\n\n", t_r);
+    printf("my      : \n%s\n\n", m_r);
+    printf("length of copies:\n");
+    printf(" original: |    my:\n");
+    printf("    %lu         %lu\n", strlen(t_r), strlen(m_r));
+    free(t_r);
+    free(m_r);
+
+    printf("\n\n	FT_STRLEN\n");
+    char *string = "12321";
+    printf("TEST 1. STRING: %s\n", string);
+    printf(" original: |    my:\n");
+    printf("    %lu         %lu\n", strlen(string), ft_strlen(string));
+
+    string = "";
+    printf("\nTEST 2. STRING: >>EMPTY<<%s\n", string);
+    printf(" original: |    my:\n");
+    printf("    %lu         %lu\n", strlen(string), ft_strlen(string));
+
+    printf("\nTEST 3. LONG STRING: %s\n\n", l_s);
+    printf(" original: |    my:\n");
+    printf("    %lu         %lu\n", strlen(l_s), ft_strlen(l_s));
+
+
+    printf("\n\n	FT_READ\n");
+    printf("TEST 1. From file __mfile__\n");
+    char *m_buff = calloc(50, sizeof (char));
+    char *t_buff = calloc(50, sizeof (char));
+    int fd = open("mfile", O_RDONLY);
+   // int m_readbytes = ft_read(fd, m_buff, 5);
+    int t_readbytes = read(fd, t_buff, 5);
+    printf(" original: |    my:\n");
+    printf("  %s        %s\n", t_buff, m_buff);
+
     return 0;
 }
